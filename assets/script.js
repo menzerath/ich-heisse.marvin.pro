046fc400f6de075200f7008843da00d497463f01
Marvin Menzerath <marvin.menzerath@gmx.de>
2015-04-05 14:51:45 +0200

    Initial Upload

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var audio = new Audio("assets/sound.mp3");
+
+window.onload = function() {
+	audio.play();
+};
+
+document.onclick = function() {
+	audio.play();
+};
+
+document.ondblclick = function() {
+	window.location.href = "https://www.youtube.com/watch?v=oKqqRrITlWg&list=PLJcXulNv_GYgmiIYoG4cMJPinVQOR8XEn";
+};
