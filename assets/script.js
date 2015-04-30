d2975b61bd7f05f7e8e572869ecaea09a42133b5
Marvin Menzerath <marvin.menzerath@gmx.de>
2015-04-30 16:33:02 +0200

    Reformatted CSS and JS

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,22 @@
 //audio.loop = true;
 var sound = new Audio("assets/sound.mp3");
 
-window.onload = function () {
+window.onload = function() {
 	//audio.play();
 
 	//setTimeout(function() {
 	sound.play();
 
-	setInterval(function () {
+	setInterval(function() {
 		sound.play();
 	}, 10000);
 	//}, 3000);
 };
 
-document.onclick = function () {
+document.onclick = function() {
 	sound.play();
 };
 
-document.ondblclick = function () {
+document.ondblclick = function() {
 	window.location.href = "https://www.youtube.com/watch?v=oKqqRrITlWg&list=PLJcXulNv_GYgmiIYoG4cMJPinVQOR8XEn";
 };
